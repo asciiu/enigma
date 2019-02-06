e0b1b9f05778ee09eb73f5b96190c2eddccb7812
Flowmaster <ellyssin.gimhae@gmail.com>
2019-02-05 22:00:23 -0700

    Example post.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,23 @@ extern crate router;
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use std::io::Read;
+
+fn handler_two(req: &mut Request) -> IronResult<Response> {
+    let mut payload = String::new();
+
+    // read the POST body
+    req.body.read_to_string(&mut payload).unwrap();
+    println!("{:?}", payload);
+
+    Ok(Response::with((status::Ok, "")))
+}
 
 fn main() {
     let mut router = Router::new();           // Alternative syntax:
     router.get("/", handler, "index");        // let router = router!(index: get "/" => handler,
     router.get("/:query", handler, "query");  //                      query: get "/:query" => handler);
+    router.post("/testpost", handler_two, "post");
 
     Iron::new(router).http("localhost:3000").unwrap();
 
