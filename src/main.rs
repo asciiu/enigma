c304c88544642176c43928c19d4a77462e711cfc
Flowmaster <ellyssin.gimhae@gmail.com>
2019-02-14 21:44:56 -0700

    Clean cargo deps and generate keypair via generate_keypair function.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 extern crate iron;
 extern crate router;
-extern crate rand;
 extern crate secp256k1;
 extern crate bitcoin;
 
-use iron::prelude::*;
-use iron::status;
-use router::Router;
-use std::io::Read;
-use secp256k1::{Secp256k1, SecretKey, PublicKey};
+//use iron::prelude::*;
+//use iron::status;
+//use router::Router;
+//use std::io::Read;
 
 use bitcoin::network::constants::Network;
 use bitcoin::util::address::Payload;
 use bitcoin::util::address::Address;
+use secp256k1::Secp256k1;
+use secp256k1::key::PublicKey;
+//use secp256k1::rand;
+use secp256k1::rand::OsRng;
 
-fn handler_two(req: &mut Request) -> IronResult<Response> {
-    let mut payload = String::new();
+// fn handler_two(req: &mut Request) -> IronResult<Response> {
+//     let mut payload = String::new();
 
-    // read the POST body
-    req.body.read_to_string(&mut payload).unwrap();
-    println!("{:?}", payload);
+//     // read the POST body
+//     req.body.read_to_string(&mut payload).unwrap();
+//     println!("{:?}", payload);
 
-    Ok(Response::with((status::Ok, "")))
-}
+//     Ok(Response::with((status::Ok, "")))
+// }
 
 fn main() {
     //let mut router = Router::new();           // Alternative syntax:
@@ -36,14 +38,12 @@ fn main() {
     //    let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
     //    Ok(Response::with((status::Ok, *query)))
     //}
- 
     let network = Network::Bitcoin;
  
     // Generate random key pair
     let s = Secp256k1::new();
-    //let (secret_key, public_key) = s.generate_keypair(&mut thread_rng());
-    let secret_key = SecretKey::from_slice(&[0xcd; 32]).expect("32 bytes, within curve order");
-    let public_key = PublicKey::from_secret_key(&s, &secret_key);
+    let mut rng = OsRng::new().expect("OsRng");
+    let (secret_key, public_key) = s.generate_keypair(&mut rng);
  
     // Generate pay-to-pubkey address
     let address = Address::p2pk(&public_key, network);
