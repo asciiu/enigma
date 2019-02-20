93f2bbab3f4f116cd3e5a7b5228c7110efd86152
Flowmaster <ellyssin.gimhae@gmail.com>
2019-02-19 21:03:58 -0700

    Implemented parse_form.

--- src/main.rs
+++ src/main.rs
@@ -12,41 +12,59 @@ use hyper::server::{Request, Response, Service};
 use futures::Stream;
 use futures::future::{Future, FutureResult};
 
+use std::collections::HashMap;
+use std::io;
+
+#[allow(dead_code)]
 struct NewMessage {
     username: String,
     message: String,
 }
 
 fn parse_form(form_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
-    futures::future::ok(NewMessage {
-        username: String::new(),
-        message: String::new(),
-    })
+    let mut form = url::form_urlencoded::parse(form_chunk.as_ref())
+        .into_owned()
+        .collect::<HashMap<String, String>>();
+
+    if let Some(message) = form.remove("message") {
+        let username = form.remove("username").unwrap_or(String::from("anonymous"));
+        let message = NewMessage { 
+            username: username, 
+            message: message
+        };
+
+        futures::future::ok(message)
+    } else {
+        futures::future::err(hyper::Error::from(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Missing field 'message",
+        )))
+    }
 }
 
-fn write_to_db(entry: NewMessage) -> FutureResult<i64, hyper::Error> {
+fn write_to_db(_entry: NewMessage) -> FutureResult<i64, hyper::Error> {
     futures::future::ok(0)
 }
 
 fn make_post_response(
-    result: Result<i64, hyper::Error>,
+    _result: Result<i64, hyper::Error>,
 ) -> FutureResult<hyper::Response, hyper::Error> {
     futures::future::ok(Response::new().with_status(StatusCode::NotFound))
 }
 
 struct Microservice;
 
+impl Microservice {
+    fn new() -> Microservice {
+        Microservice{}
+    }
+}
+
 impl Service for Microservice {
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
-
-    //fn call(&self, request: Request) -> Self::Future {
-    //    info!("Microservice received a request: {:?}", request);
-
-    //    Box::new(futures::future::ok(Response::new()))
-    //}
 
     fn call(&self, request: Request) -> Self::Future {
         match (request.method(), request.path()) {
@@ -59,9 +77,18 @@ impl Service for Microservice {
                     .then(make_post_response);
                 Box::new(future)
             }
-            _ => Box::new(futures::future::ok(
-                Response::new().with_status(StatusCode::NotFound),
-            )),
+            (&Get, "/") => {
+                info!("{} {} {}", request.method(), request.uri(), StatusCode::Ok);
+                Box::new(futures::future::ok(
+                    Response::new().with_status(StatusCode::Ok),
+                ))
+            }
+            _ => {
+                info!("{} {} {}", request.method(), request.uri(), StatusCode::NotFound);
+                Box::new(futures::future::ok(
+                    Response::new().with_status(StatusCode::NotFound),
+                ))
+            },
         }
     }
 }
@@ -70,7 +97,7 @@ fn main() {
     env_logger::init();
     let address = "127.0.0.1:8080".parse().unwrap();
     let server = hyper::server::Http::new()
-        .bind(&address, || Ok(Microservice {}))
+        .bind(&address, || Ok(Microservice::new()))
         .unwrap();
     info!("Running microservice at {}", address);
     server.run().unwrap();
